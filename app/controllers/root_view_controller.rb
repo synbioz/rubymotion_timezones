de9bbe4b11d6bf8217099c8d2fb8a0fb531fdb80
Nicolas Cavigneaux <nico@bounga.org>
2013-07-09 13:34:59 +0200

    Add remote time label and binding

--- app/controllers/root_view_controller.rb
+++ app/controllers/root_view_controller.rb
@@ -3,9 +3,11 @@ class RootViewController < UIViewController
     view.backgroundColor = UIColor.scrollViewTexturedBackgroundColor
 
     @text_field = name_text_field
+    @remote_time_label = remote_time_label
 
     view.addSubview name_label
     view.addSubview @text_field
+    view.addSubview @remote_time_label
 
     single_tap = UITapGestureRecognizer.alloc.initWithTarget(self, action: :'handle_single_tap')
     view.addGestureRecognizer(single_tap)
@@ -13,6 +15,7 @@ class RootViewController < UIViewController
 
   def textFieldShouldReturn(text_field)
     text_field.resignFirstResponder
+    @remote_time_label.text = "Bonjour #{text_field.text} !"
   end
 
   def handle_single_tap
@@ -37,5 +40,14 @@ class RootViewController < UIViewController
     textField.delegate = self
 
     textField
+  end
+
+  def remote_time_label
+    label = UILabel.alloc.initWithFrame [[0, 350], [view.frame.size.width, 30]]
+    label.backgroundColor = UIColor.clearColor
+    label.textColor = UIColor.whiteColor
+    label.textAlignment = NSTextAlignmentCenter
+
+    label
   end
 end
