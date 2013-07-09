da8cc2445163f18c7faa8d8dea34efbebfddf0f2
Nicolas Cavigneaux <nico@bounga.org>
2013-07-09 12:16:17 +0200

    Add label and text field for name

---
+++ app/controllers/root_view_controller.rb
@@ -0,0 +1,26 @@
+class RootViewController < UIViewController
+  def viewDidLoad
+    view.backgroundColor = UIColor.scrollViewTexturedBackgroundColor
+
+    view.addSubview name_label
+    view.addSubview name_text_field
+  end
+
+  private
+
+  def name_label
+    label = UILabel.alloc.initWithFrame [[10, 10], [100, 30]]
+    label.backgroundColor = UIColor.clearColor
+    label.textColor = UIColor.whiteColor
+    label.text = "Votre nom"
+    label
+  end
+
+  def name_text_field
+    textField = UITextField.alloc.initWithFrame [[110, 10], [170, 30]]
+    textField.borderStyle = UITextBorderStyleRoundedRect
+    textField.font = UIFont.systemFontOfSize(15)
+
+    textField
+  end
+end
